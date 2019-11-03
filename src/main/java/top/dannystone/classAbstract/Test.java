5caad2c4dc32e0705158c8ba41b888ea9cf80808
daniel <daniel.zhang@ipiaoniu.com>
2019-11-03 22:08:28 +0800

    r

---
+++ src/main/java/top/dannystone/classAbstract/Test.java
@@ -0,0 +1,22 @@
+package top.dannystone.classAbstract;
+
+import lombok.Data;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:it seems abstract contains non or not-non abstract methods
+ *
+ * @author: daniel
+ * @creed: keep it simple and stupid !
+ * @Time: 2019/11/3 7:38 PM
+ */
+class Test {
+    abstract public class  A{
+
+    }
+
+    @Data
+    class B extends A{
+
+    }
+}
